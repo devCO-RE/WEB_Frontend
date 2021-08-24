bb33b5f93f3d2d63bcd65796e15de2b6c6af8007
Yeongin Park <pyi7628@gmail.com>
2021-08-25 00:19:55 +0900

    feat: Route Main and Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import "./App.css";
+import { Main, Login } from "./page";
 function App() {
+  const [auth, setAuth] = useState<any>({
+    isAdmin: null,
+    token: null,
+  });
+  useEffect(
+    () =>
+      setAuth({
+        isAdmin: localStorage.getItem("isAdmin"),
+        token: localStorage.getItem("token"),
+      }),
+    []
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/Login" component={Login} />
+          <Route path="/Main" component={Main} />
+          <Route
+            path="/"
+            render={() =>
+              auth.token !== null ? (
+                <Redirect
+                  to={{
+                    pathname: "/Main",
+                  }}
+                />
+              ) : (
+                <Redirect
+                  to={{
+                    pathname: "/Login",
+                  }}
+                />
+              )
+            }
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/page/Login/index.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+
+function Login() {
+  return <div>Login page</div>;
+}
+export default Login;
