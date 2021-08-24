4e44b1e2755065b615c225f57aa272ac24eb2142
Yeongin Park <pyi7628@gmail.com>
2021-08-25 03:45:30 +0900

    feat: Make dashboard template

---
+++ src/components/organisms/index.ts
@@ -0,0 +1,4 @@
+export { mainListItems, secondaryListItems} from './ListItems';
+export {default as MaterialTable} from './MaterialTable';
+export {default as Deposits} from './Deposits';
+export {default as Chart} from './Chart';
